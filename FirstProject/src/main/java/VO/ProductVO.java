00c1e7fc9dfd97e0c4dee42381acbebac958c6b3
smhrd <smhrd@DESKTOP-4PI3G0H>
2021-08-13 12:55:46 +0900

    Added Product VO

---
+++ FirstProject/src/main/java/VO/ProductVO.java
@@ -0,0 +1,70 @@
+package VO;
+
+public class ProductVO {
+
+	 private String pdtCode;
+	 private String pdtName;
+	 private String pdtBrand;
+	 private String pdtCountry;
+	 private String pdtPrice;
+	 private String pdtDiscPrice;
+	 private String pdtType;
+	 private String pdtDailyIntake;
+	 private String pdtOneIntake;
+	 private String pdtFree;
+	 private String pdtContent;
+	 
+	public String getPdtCode() {
+		return pdtCode;
+	}
+	public String getPdtName() {
+		return pdtName;
+	}
+	public String getPdtBrand() {
+		return pdtBrand;
+	}
+	public String getPdtCountry() {
+		return pdtCountry;
+	}
+	public String getPdtPrice() {
+		return pdtPrice;
+	}
+	public String getPdtDiscPrice() {
+		return pdtDiscPrice;
+	}
+	public String getPdtType() {
+		return pdtType;
+	}
+	public String getPdtDailyIntake() {
+		return pdtDailyIntake;
+	}
+	public String getPdtOneIntake() {
+		return pdtOneIntake;
+	}
+	public String getPdtFree() {
+		return pdtFree;
+	}
+	public String getPdtContent() {
+		return pdtContent;
+	}
+	public ProductVO(String pdtCode, String pdtName, String pdtBrand, String pdtCountry, String pdtPrice,
+			String pdtDiscPrice, String pdtType, String pdtDailyIntake, String pdtOneIntake, String pdtFree,
+			String pdtContent) {
+		super();
+		this.pdtCode = pdtCode;
+		this.pdtName = pdtName;
+		this.pdtBrand = pdtBrand;
+		this.pdtCountry = pdtCountry;
+		this.pdtPrice = pdtPrice;
+		this.pdtDiscPrice = pdtDiscPrice;
+		this.pdtType = pdtType;
+		this.pdtDailyIntake = pdtDailyIntake;
+		this.pdtOneIntake = pdtOneIntake;
+		this.pdtFree = pdtFree;
+		this.pdtContent = pdtContent;
+	}
+	
+
+	 
+	 
+}
